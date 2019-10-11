4c43c983b84f916874bfca0700f73ef0f19daed9
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-10-11 02:59:34 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,8 +6,8 @@ class Backer
    @backed_projects = []
   end
   
-  def back_project(Project, backer)
-    @backed_projects << Project
-    Project.backers << backer
+  def back_project(project, backer)
+    @backed_projects << project
+    project.
   end
 end
